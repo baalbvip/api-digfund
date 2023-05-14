063d42a39fd6b8c0fdd26af5c01961175543b1ed
= <=>
2023-05-14 15:13:19 -0400

    sadas

--- App/Class/Get.php
+++ App/Class/Get.php
@@ -80,6 +80,9 @@ class Get
 
         $session = CheckSession();
         if ($session) {
+
+            NewLog($session);
+            NewLog($code);
             return DB::procedure("EXECUTE dbo.SP_POR_Detalle_Inversiones @serie = '$code' , @cod_cuenta = '$session'");
         }
     }
