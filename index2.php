d90d37ce250e568c39d061d2f72f56aa9f57dec5
Baalbvip <baalbvip@gmail.com>
2023-05-29 07:52:44 -0400

    asdsadsa

--- index2.php
+++ index2.php
@@ -21,11 +21,8 @@ $arr = explode("\n", $output);
 
 
 foreach ($arr as $file) {
-    $str = "";
-    $pos = strpos($file, "ED_") .  $str = "ED_";  
-
-  
-    $pos == 0 ?  $pos = strpos($file, "ED_") . $str = "ED_" : '';
+    $str = "ED_";
+    $pos = strpos($file, "ED_");  
 
     if ($pos !== false) {
         $filename = substr($file, $pos + 3);  // Obtener la porción de la cadena después de "EC_"
