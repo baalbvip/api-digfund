fe3b938c596e8d9fdaa4fae4bdc7a832a7645d42
Baalbvip <baalbvip@gmail.com>
2023-05-29 08:01:30 -0400

    asdas

--- index2.php
+++ index2.php
@@ -5,6 +5,8 @@ $ftpPassword = '9ov%1y72DIG#';
 
 $remoteDirectory = 'https://achieveprocessingcenter.com/ACRepository/';
 
+$year = "2021";
+
 // Comando FTP para obtener la lista de archivos
 $command = "ftp -n $ftpServer <<END_SCRIPT
 quote USER $ftpUsername
@@ -18,6 +20,8 @@ $output = shell_exec($command);
 
 // Imprimir la salida
 $arr = explode("\n", $output);
+
+$months = ["01" => [], "02" => [], "03" => [], "04" => [], "05" => [], "06" => [], "07" => [], "08" => [], "09" => [], "10" => [], "11" => [], "12" => []];
 
 
 foreach ($arr as $file) {
@@ -33,9 +37,11 @@ foreach ($arr as $file) {
         $archivoAnio = substr($fileName, 7, 4);
         $archivoPortafolio = substr($fileName, 16, 5);
 
-
-
-        print($archivoTipo);
-        print($archivoMes);
+        if ($archivoAnio == $year) {
+            $urlArchivo = $remoteDirectory . $fileName;
+            $months[$archivoMes][] = ['url_download' => $urlArchivo];
+        }
     }
 }
+
+print_r($months);
