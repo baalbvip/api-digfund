5df510dc3bbad04b0c68b5b6146c0ee988115555
Baalbvip <baalbvip@gmail.com>
2023-05-29 06:59:47 -0400

    sads

--- index2.php
+++ index2.php
@@ -1,22 +1,34 @@
 <?php
 
-
 $ftpServer = 'achieveprocessingcenter.com';
 $ftpUsername = 'integraciondig';
 $ftpPassword = '9ov%1y72DIG#';
 $remoteDirectory = 'https://achieveprocessingcenter.com/ACRepository/';
 
-
 $conn = ftp_connect($ftpServer);
-
 
 if (!$conn) {
     die("Error al conectarse con el servidor");
 }
 
-
 if (!ftp_login($conn, $ftpUsername, $ftpPassword)) {
     die("Error de inicio de sesión FTP");
 } else {
-    print "ok Todo bien";
+    print "Conexión FTP exitosa\n";
 }
+
+// Obtener lista de archivos en el directorio remoto
+$fileList = ftp_nlist($conn, "/");
+
+if ($fileList === false) {
+    die("Error al obtener la lista de archivos");
+}
+
+// Imprimir la lista de archivos
+foreach ($fileList as $file) {
+    echo $file . "\n";
+}
+
+// Cerrar la conexión FTP
+ftp_close($conn);
+?>
