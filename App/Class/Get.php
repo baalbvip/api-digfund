fcd0df1ebcebad9c50b6b51b6e84942b8105e13d
Baalbvip <baalbvip@gmail.com>
2023-05-29 08:53:44 -0400

    asdsa

--- App/Class/Get.php
+++ App/Class/Get.php
@@ -136,51 +136,56 @@ class Get
 
     static function StatusAccount()
     {
-
-        $ftpServer = 'achieveprocessingcenter.com';
-        $ftpUsername = 'integraciondig';
-        $ftpPassword = '9ov%1y72DIG#';
-
-        $remoteDirectory = 'https://achieveprocessingcenter.com/ACRepository/';
-
-        $year = "2021";
-
-        // Comando FTP para obtener la lista de archivos
-        $command = "ftp -n $ftpServer <<END_SCRIPT
-        quote USER $ftpUsername
-        quote PASS $ftpPassword
-        ls -p
-        quit
-        END_SCRIPT";
-
-        // Ejecutar el comando y capturar la salida
-        $output = shell_exec($command);
-
-        // Imprimir la salida
-        $arr = explode("\n", $output);
-
         $months = ["01" => [], "02" => [], "03" => [], "04" => [], "05" => [], "06" => [], "07" => [], "08" => [], "09" => [], "10" => [], "11" => [], "12" => []];
 
+        try {
+            $ftpServer = 'achieveprocessingcenter.com';
+            $ftpUsername = 'integraciondig';
+            $ftpPassword = '9ov%1y72DIG#';
 
-        foreach ($arr as $file) {
-            $str = "ED_";
-            $pos = strpos($file, "ED_");
+            $remoteDirectory = 'https://achieveprocessingcenter.com/ACRepository/';
 
-            if ($pos !== false) {
-                $filename = substr($file, $pos + 3);  // Obtener la porción de la cadena después de "EC_"
-                $filename = $str . $filename;
-                $fileName = basename($filename);
-                $archivoTipo = substr($fileName, 0, 3);
-                $archivoMes = substr($fileName, 5, 2);
-                $archivoAnio = substr($fileName, 7, 4);
-                $archivoPortafolio = substr($fileName, 16, 5);
+            $year = "2021";
 
-                if ($archivoAnio == $year) {
-                    $urlArchivo = $remoteDirectory . $fileName;
-                    $months[$archivoMes][] = ['url_download' => $urlArchivo];
+            // Comando FTP para obtener la lista de archivos
+            $command = "ftp -n $ftpServer <<END_SCRIPT
+            quote USER $ftpUsername
+            quote PASS $ftpPassword
+            ls -p
+            quit
+            END_SCRIPT";
+
+            // Ejecutar el comando y capturar la salida
+            $output = shell_exec($command);
+
+            // Imprimir la salida
+            $arr = explode("\n", $output);
+
+
+
+            foreach ($arr as $file) {
+                $str = "ED_";
+                $pos = strpos($file, "ED_");
+
+                if ($pos !== false) {
+                    $filename = substr($file, $pos + 3);  // Obtener la porción de la cadena después de "EC_"
+                    $filename = $str . $filename;
+                    $fileName = basename($filename);
+                    $archivoTipo = substr($fileName, 0, 3);
+                    $archivoMes = substr($fileName, 5, 2);
+                    $archivoAnio = substr($fileName, 7, 4);
+                    $archivoPortafolio = substr($fileName, 16, 5);
+
+                    if ($archivoAnio == $year && $archivoPortafolio == "00364") {
+                        $urlArchivo = $remoteDirectory . $fileName;
+                        $months[$archivoMes][] = ['url_download' => $urlArchivo];
+                    }
                 }
             }
+        } catch (Exception $e) {
+            print_r($e);
         }
+
 
         return $months;
     }
