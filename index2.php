39696927a7e2168ff64291919fb48bcd98ce0cf6
Baalbvip <baalbvip@gmail.com>
2023-05-29 07:13:26 -0400

    asdas

--- index2.php
+++ index2.php
@@ -3,6 +3,12 @@ $ftpServer = 'achieveprocessingcenter.com';
 $ftpUsername = 'integraciondig';
 $ftpPassword = '9ov%1y72DIG#';
 
+
+$tipo = 'ED_';
+$mes = '12';
+$anio = '2022';
+$portafolio = '00364';
+
 // Comando FTP para obtener la lista de archivos
 $command = "ftp -n $ftpServer <<END_SCRIPT
 quote USER $ftpUsername
@@ -15,6 +21,28 @@ END_SCRIPT";
 $output = shell_exec($command);
 
 // Imprimir la salida
-$arr = explode("\n",$output);
+$arr = explode("\n", $output);
 
-print_r($arr);
+
+foreach ($arr as $file) {
+    $fileName = basename($file);
+    $archivoTipo = substr($fileName, 0, 3);
+    $archivoMes = substr($fileName, 5, 2);
+    $archivoAnio = substr($fileName, 7, 4);
+    $archivoPortafolio = substr($fileName, 16, 5);
+
+    // Filtrar archivos basado en las variables
+    if (
+        $archivoTipo === $tipo &&
+        $archivoMes === $mes &&
+        $archivoAnio === $anio &&
+        $archivoPortafolio === $portafolio
+    ) {
+
+        // Generar URL del archivo FTP
+        $urlArchivo = $remoteDirectory . $fileName;
+
+        // Mostrar el nombre del archivo como hipervínculo
+        echo '<a href="' . $urlArchivo . '" target="_blank">' . $fileName . '</a><br>';
+    }
+}
