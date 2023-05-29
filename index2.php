2e227b1379bd43a9880e8c6e1317d35750d73f00
Baalbvip <baalbvip@gmail.com>
2023-05-29 07:44:56 -0400

    asdsa

--- index2.php
+++ index2.php
@@ -21,9 +21,12 @@ $arr = explode("\n", $output);
 
 
 foreach ($arr as $file) {
-    $pos = strpos($file, "EC_");  // Obtener la posición de la primera aparición de "EC_"
+    $str = "";
+    $pos = strpos($file, "EC_") .  $str = "EC_";  // Obtener la posición de la primera aparición de "EC_"
+    $pos == false ?  ($pos = strpos($file, "ED_") . $str = "ED_") : '';
+
     if ($pos !== false) {
         $filename = substr($file, $pos + 3);  // Obtener la porción de la cadena después de "EC_"
-        echo $filename . "\n";
+        echo $str . $filename . "\n";
     }
 }
