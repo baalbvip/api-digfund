7ea7f7ae848ed3ae640a7dc6b40bc9f322cba4bb
Baalberith <72678093+baalbvip@users.noreply.github.com>
2023-07-09 11:43:43 -0400

    sdass

--- App/Functions/Global.php
+++ App/Functions/Global.php
@@ -1,5 +1,5 @@
 <?php
-require("../../vendor/autoload.php");
+require($_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php");
 
 use Automattic\WooCommerce\Client;
 use Automattic\WooCommerce\HttpClient\HttpClientException;
