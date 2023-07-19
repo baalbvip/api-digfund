2e7171838915f9f75013ce9aacedd870418d4840
Baalberith <72678093+baalbvip@users.noreply.github.com>
2023-07-19 08:11:21 -0400

    sadsa

--- App/Functions/Global.php
+++ App/Functions/Global.php
@@ -120,7 +120,7 @@ function CreateUser($params)
             [$first_name, $last_name, $email, $email, "default", "E", "1", $email, date("Y-m-d"), $num_port]
         );*/
 
-        $procedure = DB::procedure("EXECUTE dbo.SP_WEB_REGISTRAR_CUENTA_IN @pNombre = '$first_name', @pApellidos = '$last_name', @pCorreo = '$email',@pDireccion = '$country',@pTelefono = '$phone',@pUsuario = '$email',@pContrasenna = 'default',@pTipo = 'E', @pUsuario_Registro = '" . DateTime() . "', @pError = 'x',@nom_cuenta = '$first_name',@fec_apertura = '" . DateTime() . "',@cod_ejecutivo = '$asesor', @pNum_Portafolio = '$num_port', @tipoOperacion = '1'");
+        $procedure = DB::procedure("EXECUTE dbo.SP_WEB_REGISTRAR_CUENTA_IN @pNombre = '$first_name', @pApellidos = '$last_name', @pCorreo = '$email',@pDireccion = '$country',@pTelefono = '$phone',@pUsuario = '$email',@pContrasenna = 'default',@pTipo = 'E', @pUsuario_Registro = 'WEB-DIG', @pError = 'x',@nom_cuenta = '$first_name',@fec_apertura = '" . DateTime() . "',@cod_ejecutivo = '$asesor', @pNum_Portafolio = '$num_port', @tipoOperacion = '1'");
         $id = ExistsUser($email)['Id_Usuario'];
 
         NewLog("Usuario creado con exito $email");
